f7a3795129be35b7d7ffc7161bad3b9aff19ef79
Erie-Mac <ditrich13@gmail.com>
2018-05-24 09:22:57 +0300

    Socket config updates

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -7,7 +7,6 @@ use Ratchet\ConnectionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class ChatController extends Controller implements MessageComponentInterface {
     protected $users;
 
@@ -15,16 +14,15 @@ class ChatController extends Controller implements MessageComponentInterface {
         $this->users = new \SplObjectStorage;
     }
 
-    public function onOpen(ConnectionInterface $conn) {
-        // Attach new connection
-        $this->users->attach($conn); /// - 
-
-        echo "New user! ({$conn->resourceId})\n";
+    public function onOpen(ConnectionInterface $socket) {
+        // Attach new socketection
+        $this->users->attach($socket);
+        echo "New user! ({$socket->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->users) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+        echo sprintf('Connection %d sending message "%s" to %d other socketection%s' . "\n"
             , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->users as $user) {
@@ -34,20 +32,18 @@ class ChatController extends Controller implements MessageComponentInterface {
         }
     }
 
-    public function onClose(ConnectionInterface $conn) {
-        $this->users->detach($conn);
-
-        echo "Connection {$conn->resourceId} was terminated\n";
+    public function onClose(ConnectionInterface $socket) {
+        $this->users->detach($socket);
+        echo "Connection {$socket->resourceId} was terminated\n";
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
+    public function onError(ConnectionInterface $socket, \Exception $e) {
         echo "You got an error: {$e->getMessage()}\n";
-
-        $conn->close();
+        $socket->close();
     }
 
     /**
-     * @Route("/", name="chat")
+     * @Route("/", name="index")
      */
     public function index()
     {
