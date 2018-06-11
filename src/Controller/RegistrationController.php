c87e38c5d65cab7bc82cab7d92a68549d673b717
Erie-Mac <ditrich13@gmail.com>
2018-06-12 01:40:59 +0300

    Redirect to login

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends Controller
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('/admin');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render(
